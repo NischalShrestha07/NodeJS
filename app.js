b210517016b00c3da9576d48898e069622610685
Nischal <nischalshrestha800@gmail.com>
2024-04-17 22:59:55 +0545

    .

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+const { name } = require('ejs')
 const express=require('express')
 const app=express()
 
+app.set('view engine','ejs')
+
+
+// app.get('/',(req,res)=>{
+// res.send("This is a home page")
+// })
 app.get('/',(req,res)=>{
-res.send("This is a home page")
+// res.render('./views/home.ejs') this throws errors 
+
+
+// res.render('home.ejs')
+res.render('home',{name:'Nischal',age:'21'})
 })
 
 app.get('/about',(req,res)=>{
-res.send("<h1>This is about page</h1>")
+res.send("<h1>This is about page</h1><h2>This is h2</h2><img src='me.jpg'>")
 })
 
 app.get('/blog',(req,res)=>{
@@ -31,3 +42,6 @@ res.send("So, Here we will gona do editing")
 app.listen(3000,()=>{
     console.log("Server has started at port 3000");
 })
+
+//1.nodemon
+//2. node --watch app.js  then when we refresh it will work
