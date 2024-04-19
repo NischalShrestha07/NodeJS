6e934a6c5d181932458083f39b368210523ef14d
Nischal <nischalshrestha800@gmail.com>
2024-04-19 22:08:45 +0545

    ui complete

--- app.js
+++ app.js
@@ -21,12 +21,14 @@ res.send("<h1>This is about page</h1><h2>This is h2</h2><img src='me.jpg'>")
 })
 
 app.get('/blog',(req,res)=>{
-res.send("Welcome to blog page")
+// res.send("Welcome to blog page")
+res.render('blog')
 })
 
 // rest apis
 app.get('/createblog',(req,res)=>{
-res.send("So, this is a create blog")
+// res.send("So, this is a create blog")
+res.render("createblog")
 })
 app.get('/create/blog',(req,res)=>{
 res.send("So, this is a inside create then blog")
@@ -34,7 +36,8 @@ res.send("So, this is a inside create then blog")
 
 // restful apis
 app.get('/blog/edit',(req,res)=>{
-res.send("So, Here we will gona do editing")
+// res.send("So, Here we will gona do editing")
+res.render('editblog')
 })
 
 
